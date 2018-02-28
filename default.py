efd49f8eb9b2ac9502fce7eb7ce335061797e7ad
Yogesh Pawar <yogeshpawar159@gmail.com>
2018-02-28 14:40:14 +0530

    Implemented Latest Link Completely

--- default.py
+++ default.py
@@ -1,13 +1,39 @@
 # from resources.lib.ui import control
-# from resources.lib.ui import utils
-# from resources.lib.ui.SourcesList import SourcesList
+from resources.lib.ui import utils
+from resources.lib.ui.SourcesList import SourcesList
 # from resources.lib.ui.router import route, router_process
-# from resources.lib.NineAnimeBrowser import NineAnimeBrowser
+from resources.lib.NineAnimeBrowser import NineAnimeBrowser
 # import urlparse
 
 from xbmcswift2 import Plugin
 
 plugin = Plugin()
+SERVER_CHOICES = {
+    "serverg4": "Server G4",
+    "serverrapid": "RapidVideo",
+    "servermycloud": "MyCloud",
+    "serveropenload": "OpenLoad",
+}
+
+_BROWSER = NineAnimeBrowser()
+
+
+def isDirectoryStyle():
+    style = plugin.get_setting('displaystyle')
+    return "Directory" == style
+
+
+def sortResultsByRes(fetched_urls):
+    prefereResSetting = utils.parse_resolution_of_source(plugin.get_setting('prefres'))
+
+    filtered_urls = filter(lambda x: utils.parse_resolution_of_source(x[0]) <=
+                                     prefereResSetting, fetched_urls)
+
+    return sorted(filtered_urls, key=lambda x:
+    utils.parse_resolution_of_source(x[0]),
+                  reverse=True)
+
+
 '''
 AB_LIST = [".", "0"] + [chr(i) for i in range(ord("A"), ord("Z") + 1)]
 MENU_ITEMS = [
@@ -205,15 +231,149 @@ def LIST_MENU(payload):
 
 # router_process(control.get_plugin_url())
 '''
+MENU_ITEMS = [
+    ("Latest", "latest"),
+    ("Newest", "newest"),
+    ("Recent Subbed", "recent_subbed"),
+    ("Popular Subbed", "popular_subbed"),
+    ("Recent Dubbed", "recent_dubbed"),
+    ("Popular Dubbed", "popular_dubbed"),
+    ("Genres", "genres"),
+    ("Search", "search"),
+    ("Settings", "settings")
+]
+
+
+# @plugin.route("/latest", name="default", options={"page": "0"})
+@plugin.route("/latest")
+def latest():
+    try:
+        page = plugin.request.args["page"][0]
+    except KeyError:
+        page = 1
+    plugin.log.info(page)
+    animes = _BROWSER.get_latest(int(page))
+    plugin.log.info(animes[0])
+    plugin.log.info(animes[-1])
+    items = []
+    for anime in animes:
+        u = anime["url"]
+        if not u.startswith("latest"):
+            path = plugin.url_for("animes", anime=anime["url"].split("/")[-1])
+        else:
+            path = plugin.url_for("latest", page=anime["url"].split("/")[-1])
+        items.append(
+            {
+                "label": anime["name"],
+                "path": path
+            }
+        )
+    return plugin.finish(items)
+
+
+@plugin.route("/animes/<anime>")
+def animes(anime):
+    plugin.log.info(anime)
+    episodes = _BROWSER.get_anime_episodes(anime, isDirectoryStyle())
+    plugin.log.info(episodes[0])
+    plugin.log.info(episodes[-1])
+    items = []
+    for episode in episodes:
+        anime, episode_number = episode["url"].split("/")[1:]
+        items.append(
+            {
+                "label": episode["name"],
+                "path": plugin.url_for("play", anime=anime, episode_number=episode_number)
+            }
+        )
+    return plugin.finish(items)
+    pass
+
+
+@plugin.route("/play/<anime>/<episode_number>")
+def play(anime, episode_number):
+    plugin.log.info("Anime: {} Episode: {}".format(anime, episode_number))
+
+    sources = _BROWSER.get_episode_sources(anime, int(episode_number))
+    serverChoice = filter(lambda x:
+                          plugin.get_setting(x[0]) == 'true', SERVER_CHOICES.iteritems())
+    serverChoice = map(lambda x: x[1], serverChoice)
+    sources = filter(lambda x: x[0] in serverChoice, sources)
+
+    autoplay = True if 'true' in plugin.get_setting('autoplay') else False
+
+    s = SourcesList(sources, autoplay, sortResultsByRes, {
+        'title': "Fetching Sources",
+        'processing': "Processing %s",
+        'choose': "Please choose source: ",
+        'notfound': "Couldn't find eligible sources",
+    })
+    #
+    # if isDirectoryStyle():
+    #     if s._read_sources():
+    #         items = sorted(s._sources.iteritems(), key=lambda x: x[0])
+    #         items = [(title[5:], url) for title, url in items]
+    #         items = map(lambda x: utils.allocate_item(x[0], 'playlink&url='+x[1], False, ''), items)
+    #         #items = map(lambda x: utils.allocate_item(anime_name + x[0], 'playlink/&url=' + x[1], False, ''), items)
+    #         return control.draw_items(items)
+    # else:
+    link = s.get_video_link()
+    plugin.log.info("Playlink: " + link)
+    item = {"label": anime, "path": link}
+    return plugin.play_video(item)
+    # return plugin.finish([])
+    pass
+
+
+@plugin.route("/newest")
+def newest():
+    pass
+
+
+@plugin.route("/recent_subbed")
+def recent_subbed():
+    pass
+
+
+@plugin.route("/popular_subbed")
+def popular_subbed():
+    pass
+
+
+@plugin.route("/recent_dubbed")
+def recent_dubbed():
+    pass
+
+
+@plugin.route("/popular_dubbed")
+def popular_dubbed():
+    pass
+
+
+@plugin.route("/genres")
+def genres():
+    pass
+
+
+@plugin.route("/search")
+def search():
+    pass
+
+
+@plugin.route("/settings")
+def settings():
+    plugin.open_settings()
+    pass
+
 
 @plugin.route('/')
-def index():
-    item = {
-        'label': 'Hello XBMC!',
-        'path': 'http://s3.amazonaws.com/KA-youtube-converted/JwO_25S_eWE.mp4/JwO_25S_eWE.mp4',
-        'is_playable': True
-    }
-    return [item]
+def list_menu():
+    items = []
+    for name, url in MENU_ITEMS:
+        items.append({"label": name, "path": plugin.url_for(url)})
+    return items
+
 
 if __name__ == '__main__':
     plugin.run()
+    plugin.set_content("tvshows")
--- resources/lib/ui/DialogProgressWrapper.py
+++ resources/lib/ui/DialogProgressWrapper.py
@@ -1,4 +1,4 @@
-import xbmcgui
+from xbmcswift2 import xbmcgui
 
 class DialogProgressWrapper(object):
     def __init__(self, title, progress_string):
